401e5c47a9573efa4ad73f01875aa237ffb66b3e
Rez Khaleghi | PocketJack <79227158+rezkhaleghi@users.noreply.github.com>
2025-05-06 14:13:13 +0300

    Update about.rs

--- src/components/about.rs
+++ src/components/about.rs
@@ -16,6 +16,16 @@ pub fn about() -> Html {
                     <p>
                         { "When I'm not coding, you'll find me creating music, exploring new sounds, and pushing the boundaries of my creative expression." }
                     </p>
+            <p>
+                        { "Hey, I'm Reza Khaleghi, but you might know me as Pocketjack. Born on July 25, 2000, in Iran, I’m currently living in Turkey. My journey into tech started back in 2011 when I began learning computer programming. I spent a couple of years dabbling in different areas like networking and security, but it wasn’t until I found backend development that everything clicked. I started out as a freelancer and eventually moved into leading backend teams, focusing on building scalable and efficient systems.
+
+Outside of tech, I’m all about music. I picked up the bass guitar in 2013-14, then moved on to the acoustic guitar, and got hooked on rock music. In 2020, I dove into trap and hip-hop by making loops and beats, and it's become a hobby I still enjoy today.
+
+Right now, I’m totally fascinated by two things: jazz music and Rust. They’ve both completely changed how I see the world—jazz opened my ears to a whole new dimension of sound, while Rust has shifted how I think about programming, making it even more enjoyable and powerful.
+
+When I'm not coding or making music, you'll find me out in nature—whether it’s camping, hiking, or traveling. These have been passions of mine since I was a kid. If I’m back in Iran, you’ll probably catch me offroading in my old Land Rover, chasing that next adventure." }
+                    </p>
+        
                 </div>
             </div>
         </section>
