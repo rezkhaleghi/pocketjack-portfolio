9011e9d67a89cbb47330d45cf5b72dcead0eeae2
Rez Khaleghi | PocketJack <rezaxkhaleghi@gmail.com>
2025-06-02 14:24:59 +0300

    d0n3

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,11 +1,9 @@
 // src/components/mod.rs
 pub mod about;
-// pub mod home;
 pub mod client_project;
 pub mod contact;
 pub mod experience;
 pub mod footer;
-// pub mod header;
 pub mod hero;
 pub mod music;
 pub mod my_projects;
